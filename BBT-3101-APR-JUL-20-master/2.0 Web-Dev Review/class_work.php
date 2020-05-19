24f3fc5100aa976597a7b173a202672a16760f8f
unknown <sandra.rop@strathmore.edu>
2020-05-19 21:50:11 +0300

    First commit

---
+++ BBT-3101-APR-JUL-20-master/2.0 Web-Dev Review/class_work.php
@@ -0,0 +1,21 @@
+<?php
+$_100784="Elvis";
+$e100784="Osodo";
+
+$Anthony="Mwala";
+$anthony="Mwaura";
+
+$_Thiarara = 101717;
+
+$Brenda= 112487;
+
+$classmates=true;
+
+$other_irr = 1.1;
+
+$amount=500;
+$Amount="500";
+
+var_dump($Amount)
+
+ ?>
